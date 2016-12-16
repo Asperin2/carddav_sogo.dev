90b8a09eb36afd22c6dd95735b72c715e66b25ee
asperin <asperin2@gmail.com>
2016-12-16 11:25:28 +0300

    fix

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
 <?php
-/*
-function __autoload($class)
-{
-    require_once "/home/eakhmetov/sogo/carddav_sogo.dev/libs/$class.php";
-}
-*/
-/*
-spl_autoload_register(function ($class_name) {
-    include "libs/".$class_name . '.php';
-});
-*/
 include "/home/eakhmetov/sogo/carddav_sogo.dev/libs/CarddavImages.php";
 include "/home/eakhmetov/sogo/carddav_sogo.dev/libs/CarddavSogo.php";
 
